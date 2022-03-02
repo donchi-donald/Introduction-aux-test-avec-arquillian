f7c54e80d57a9cd3e2930d64181eed4bcc787e02
Donchi Fofack, Donald <Donald.Donchi-Fofack@adesso.de>
2022-03-03 00:36:10 +0100

    Créer un composant pour test simple

---
+++ src/main/java/Component.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+
+public class Component {
+    public void sendMessage(PrintStream to, String msg) {
+        to.println(message(msg));
+    }
+
+    public String message(String msg) {
+        return "Message, " + msg;
+    }
+}
