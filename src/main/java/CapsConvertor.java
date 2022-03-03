3c1d0cfbefb4befd12dcc3936a67dd97b0e0c0a6
Donchi Fofack, Donald <Donald.Donchi-Fofack@adesso.de>
2022-03-03 01:37:31 +0100

    tester les enterprise java beans

---
+++ src/main/java/CapsConvertor.java
@@ -0,0 +1,8 @@
+import javax.ejb.Stateless;
+
+@Stateless
+public class CapsConvertor {
+    public ConvertToLowerCase getLowerCase(){
+        return new ConvertToLowerCase();
+    }
+}
---
+++ src/main/java/ConvertToLowerCase.java
@@ -0,0 +1,5 @@
+public class ConvertToLowerCase {
+    public String convert(String word){
+        return word.toLowerCase();
+    }
+}
